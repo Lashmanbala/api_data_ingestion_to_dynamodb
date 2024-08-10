a445fa5d1c561c747303fa5a248774afeefcfee8
lashmanbala <lashmanbala@gmail.com>
2024-08-10 19:56:07 +0530

    Getting details of 100 repos

--- app.py
+++ app.py
@@ -3,30 +3,49 @@ import json
 import os
 
 token = os.environ.get('TOKEN')
-response = requests.get('https://api.github.com/user',
-                         headers={'Authorization':f'token {token}'})
+# response = requests.get('https://api.github.com/user',
+                        #  headers={'Authorization':f'token {token}'})
 
-content = response.content.decode('utf-8')
-user = json.loads(content)
+# content = response.content.decode('utf-8')
+# user = json.loads(content)
 
-repo_res = requests.get('https://api.github.com/repos/lashmanbala/api_data_ingestion_to_dynamodb',
-        headers = {'Authorization':f'token {token}'})
+# repo_res = requests.get('https://api.github.com/repos/lashmanbala/api_data_ingestion_to_dynamodb',
+        # headers = {'Authorization':f'token {token}'})
 
-repos = json.loads(repo_res.content.decode('utf-8'))
+# repos = json.loads(repo_res.content.decode('utf-8'))
 
-repos_list = requests.get('https://api.github.com/repositories?since=840563886',headers = {'Authorization':f'token {token}'})
+repos_res = requests.get('https://api.github.com/repositories?since=840563886',headers = {'Authorization':f'token {token}'})
 
-lst = json.loads(repos_list.content.decode('utf-8'))
+repos = json.loads(repos_res.content.decode('utf-8'))
+               
+repo_details = []
 
-for repo in lst:
-    {'id':repo['id'],'owner.login':repo['owner']['login'],'name':repo['name']}
+for repo in repos:
+    try:
+        owner = repo['owner']['login']
+        name = repo['name']
 
-repo = lst[0]
+        rd = json.loads(requests.get(f'https://api.github.com/repos/{owner}/{name}', headers={'Authorization':f'token {token}'}).content.decode('utf-8'))
 
-owner = repo['owner']['login']
-name = repo['name']
+        repo_detail = {'id': rd['id'],
+                        'node_id': rd['node_id'],
+                        'name': rd['name'],
+                        'full_name': rd['full_name'],
+                        'owner': {
+                            'login': rd['owner']['login'],
+                            'id': rd['owner']['id'],
+                            'node_id': rd['owner']['node_id'],
+                            'type': rd['owner']['type'],
+                            'site_admin': rd['owner']['site_admin']
+                            },
+                        'html_url': rd['html_url'],
+                        'description': rd['description'],
+                        'fork': rd['fork'],
+                        'created_at': rd['created_at']
+                        }
 
-repo_details = requests.get(f'https://api.github.com/repos/{owner}/{name}', headers={'Authorization':f'token {token}'})
-details = json.loads(repo_details.content.decode('utf-8'))
-print(details)
+        repo_details.append(repo_detail)
+    except:
+        pass
 
+print(len(repo_details))
