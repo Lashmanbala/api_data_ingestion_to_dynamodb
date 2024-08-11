29208ead70fa9c3b38b5e1abb666e15d04f307a4
lashmanbala <lashmanbala@gmail.com>
2024-08-11 18:26:06 +0530

    uploading the data into table

--- aws_dynamo.py
+++ aws_dynamo.py
@@ -21,6 +21,7 @@ def create_tables():
             ],
             BillingMode='PAY_PER_REQUEST'
         )
+        print(ghrepos_table)
 
         marker_table = dynamo_db.create_table(
             TableName='ghmarker',
@@ -39,7 +40,45 @@ def create_tables():
             BillingMode='PAY_PER_REQUEST'
         )
 
+        print(marker_table)
         print('Tables created successfully')
     except Exception as e:
         print(e)
 
+
+def load_repos(table_name, repo_details_list):
+    for repo in repo_details_list:
+        # Convert the item attributes to the correct DynamoDB format
+        item = {
+            'id': {'N': str(repo['id'])},  # 'N' for Number, must be a string representation of the number
+            'node_id': {'S': repo['node_id']},  # 'S' for String
+            'name': {'S': repo['name']},
+            'full_name': {'S': repo['full_name']},
+            'owner': {
+                'M': {  # 'M' for Map
+                    'login': {'S': repo['owner']['login']},
+                    'id': {'N': str(repo['owner']['id'])},
+                    'node_id': {'S': repo['owner']['node_id']},
+                    'type': {'S': repo['owner']['type']},
+                    'site_admin': {'BOOL': repo['owner']['site_admin']}  # 'BOOL' for Boolean
+                }
+            },
+            'html_url': {'S': repo['html_url']},
+            'description': {'S': repo['description'] or ''},  # Handle None by providing an empty string
+            'fork': {'BOOL': repo['fork']},
+            'created_at': {'S': repo['created_at']}
+        }
+
+        try:
+            dynamo_db.put_item(
+                TableName=table_name,
+                Item=item
+            )
+        except Exception as e:
+            print(f"Failed to insert item: {e}")
+
+    print('Successfully uploaded into table')
+
+response = dynamo_db.scan(
+    TableName='ghrepos')
+print(response)
