e4fa755fcaa43574217cf7511957d835a5848d33
lashmanbala <lashmanbala@gmail.com>
2024-08-11 08:36:25 +0530

    Modularised the code

--- app.py
+++ app.py
@@ -2,50 +2,56 @@ import requests
 import json
 import os
 
+def list_repos(token, since=840563886):
+    repos_res = requests.get(f'https://api.github.com/repositories?since={since}',
+                             headers = {'Authorization':f'token {token}'})
+    
+    return json.loads(repos_res.content.decode('utf-8'))
+
+
+def get_repo_details(owner, name, token):
+    repo_details = json.loads(requests.get(f'https://api.github.com/repos/{owner}/{name}', 
+                                          headers={'Authorization':f'token {token}'}
+                                          ).content.decode('utf-8'))
+
+    return repo_details
+
+def extract_repo_fields(repo_details):
+    repo_fields = {'id': repo_details['id'],
+                    'node_id': repo_details['node_id'],
+                    'name': repo_details['name'],
+                    'full_name': repo_details['full_name'],
+                    'owner': {
+                        'login': repo_details['owner']['login'],
+                        'id': repo_details['owner']['id'],
+                        'node_id': repo_details['owner']['node_id'],
+                        'type': repo_details['owner']['type'],
+                        'site_admin': repo_details['owner']['site_admin']
+                        },
+                    'html_url': repo_details['html_url'],
+                    'description': repo_details['description'],
+                    'fork': repo_details['fork'],
+                    'created_at': repo_details['created_at']
+                    }
+    return repo_fields
+
+def get_repos(repos, token):
+    repos_details = []
+    
+    for repo in repos:
+        try:
+            owner = repo['owner']['login']
+            name = repo['name']
+
+            repo_details = get_repo_details(owner, name, token)
+            repo_fields = extract_repo_fields(repo_details)
+            repos_details.append(repo_fields)
+        except:
+            pass
+        
+    return repos_details
+
 token = os.environ.get('TOKEN')
-# response = requests.get('https://api.github.com/user',
-                        #  headers={'Authorization':f'token {token}'})
-
-# content = response.content.decode('utf-8')
-# user = json.loads(content)
-
-# repo_res = requests.get('https://api.github.com/repos/lashmanbala/api_data_ingestion_to_dynamodb',
-        # headers = {'Authorization':f'token {token}'})
-
-# repos = json.loads(repo_res.content.decode('utf-8'))
-
-repos_res = requests.get('https://api.github.com/repositories?since=840563886',headers = {'Authorization':f'token {token}'})
-
-repos = json.loads(repos_res.content.decode('utf-8'))
-               
-repo_details = []
-
-for repo in repos:
-    try:
-        owner = repo['owner']['login']
-        name = repo['name']
-
-        rd = json.loads(requests.get(f'https://api.github.com/repos/{owner}/{name}', headers={'Authorization':f'token {token}'}).content.decode('utf-8'))
-
-        repo_detail = {'id': rd['id'],
-                        'node_id': rd['node_id'],
-                        'name': rd['name'],
-                        'full_name': rd['full_name'],
-                        'owner': {
-                            'login': rd['owner']['login'],
-                            'id': rd['owner']['id'],
-                            'node_id': rd['owner']['node_id'],
-                            'type': rd['owner']['type'],
-                            'site_admin': rd['owner']['site_admin']
-                            },
-                        'html_url': rd['html_url'],
-                        'description': rd['description'],
-                        'fork': rd['fork'],
-                        'created_at': rd['created_at']
-                        }
-
-        repo_details.append(repo_detail)
-    except:
-        pass
-
-print(len(repo_details))
+repos = list_repos(token)
+repos_details = get_repos(repos, token)
+print(len(repos_details))
