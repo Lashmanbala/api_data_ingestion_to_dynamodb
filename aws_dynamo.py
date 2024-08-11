84893b8c6be374de1d7ecb74be706c8f0c1efe6b
lashmanbala <lashmanbala@gmail.com>
2024-08-11 17:37:58 +0530

    Modularised the code

--- aws_dynamo.py
+++ aws_dynamo.py
@@ -3,44 +3,43 @@ import boto3
 
 dynamo_db = boto3.client('dynamodb')
 
-ghrepos_table = dynamo_db.create_table(
-    TableName='ghrepos',
+def create_tables():
+    try:
+        ghrepos_table = dynamo_db.create_table(
+            TableName='ghrepos',
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'id',
+                    'AttributeType': 'N'    # Numeric
+                },
+            ],
+            KeySchema=[
+                {
+                    'AttributeName': 'id',
+                    'KeyType': 'HASH'
+                },
+            ],
+            BillingMode='PAY_PER_REQUEST'
+        )
 
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'id',
-            'AttributeType': 'N'    # Numeric
-        },
-    ],
-    
-    KeySchema=[
-        {
-            'AttributeName': 'id',
-            'KeyType': 'HASH'
-        },
-    ],
+        marker_table = dynamo_db.create_table(
+            TableName='ghmarker',
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'tn',
+                    'AttributeType': 'S'    # String
+                },
+            ],
+            KeySchema=[
+                {
+                    'AttributeName': 'tn',
+                    'KeyType': 'HASH'
+                },
+            ],
+            BillingMode='PAY_PER_REQUEST'
+        )
 
-    BillingMode='PAY_PER_REQUEST'
-)
+        print('Tables created successfully')
+    except Exception as e:
+        print(e)
 
-marker_table = dynamo_db.create_table(
-    TableName='ghmarker',
-
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'tn',
-            'AttributeType': 'S'    # String
-        },
-    ],
-    
-    KeySchema=[
-        {
-            'AttributeName': 'tn',
-            'KeyType': 'HASH'
-        },
-    ],
-
-    BillingMode='PAY_PER_REQUEST'
-)
-
-print(dynamo_db.list_tables())
